42f165e4ee2cbebca93cf6b4b802fc88542abaa7
Iwan Timmer <irtimmer@gmail.com>
2014-01-11 23:48:35 +0100

    Add license

--- src/main/java/org/timmer/dolphinmote/UdpConstants.java
+++ src/main/java/org/timmer/dolphinmote/UdpConstants.java
@@ -1,8 +1,24 @@
+/*
+ *  Copyright (C) 2014 Iwan Timmer
+ *
+ *  This file is part of DolphinMote.
+ *
+ *  DolphinMote is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  DolphinMote is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.timmer.dolphinmote;
 
-/**
- * Created by iwan on 1/10/14.
- */
 public class UdpConstants {
 
 	/* Buttons*/
@@ -23,6 +39,5 @@ public class UdpConstants {
 	public final static int PACKET_IR = (1<<2);
 	public final static int PACKET_NUN = (1<<3);
 	public final static int PACKET_NUNACCEL = (1<<4);
-
 
 }
